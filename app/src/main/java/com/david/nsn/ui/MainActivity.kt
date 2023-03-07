8f15f86adef30fe3b803817c31021991f3a146d2
david <davidlab44@gmail.com>
2023-03-07 07:46:26 -0500

    object json displayed in console inside corroutine

--- app/src/main/java/com/david/nsn/ui/MainActivity.kt
+++ app/src/main/java/com/david/nsn/ui/MainActivity.kt
@@ -8,15 +8,20 @@ import com.david.nsn.ui.theme.NsnTheme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.david.nsn.data.remote.QuotesApi
 import com.david.nsn.repository.RetrofitHelper
-import com.david.nsn.util.BASE_URL
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /*
@@ -35,16 +40,16 @@ class MainActivity : AppCompatActivity() {
         // launching a new coroutine
         GlobalScope.launch {
             val result = quotesApi.getQuotes()
-            if (result != null)
-            // Checking the results
-                Log.d("response: ", result.body().toString())
+            if (result != null) {
+                // Checking the results
+                val result = result.body().toString()
+                Log.d("response: ", result)
+            }
         }
     }
+
+
 }
-
-
-
-
 
 @Composable
 fun Greeting(name: String) {
@@ -56,5 +61,29 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     NsnTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun ListWithColumn(items: String) { // (1)
+    val scrollState = rememberScrollState()
+
+    Column(modifier = Modifier.verticalScroll(scrollState)) { // (2)
+        items.forEach { item -> // (4)
+            ListItemRow(item.toString()) // (5)
+        }
+    }
+}
+
+@Composable // (3)
+fun ListItemRow(item: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Text(text = item, style = MaterialTheme.typography.bodySmall)
     }
 }
